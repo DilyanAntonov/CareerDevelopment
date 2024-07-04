2ed14f8a88144efc8638ba79e4649bd58d1a92c6
Dilyan Antonov <dilyan.y.antonov@gmail.com>
2024-07-04 23:05:49 +0300

    Add Update and Delete job listing functionality

--- career/career/companies/urls.py
+++ career/career/companies/urls.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 from career.companies import views
@@ -9,5 +8,8 @@ app_name = 'companies'
 urlpatterns = [
     path('register/', views.CompanyRegistrationView.as_view(), name='register-company'),
     path('login/', views.CompanyLoginView.as_view(), name='login-company'),
-    path('company/<int:pk>/', views.CompanyDetailView.as_view(), name='company-detail')
+    path('company/<int:pk>/', views.CompanyDetailView.as_view(), name='company-detail'),
+    path('manage-job-listings/<int:pk>/', views.ManageJobListingsView.as_view(), name='manage-job-listings'),
+    path('edit-job-listing/<int:pk>/', views.JobListingUpdateView.as_view(), name='edit-job-listing'),
+    path('delete-job-listing/<int:pk>/', views.JobListingDeleteView.as_view(), name='delete-job-listing'),
 ]
