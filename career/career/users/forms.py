b8945a03a769add1399e42ce93924bcb51462c18
Dilyan Antonov <dilyan.y.antonov@gmail.com>
2024-07-06 23:08:54 +0300

    Add update functionality for user profile

--- career/career/users/forms.py
+++ career/career/users/forms.py
@@ -1,10 +1,22 @@
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 
-from career.users.models import User
+from career.companies.models import Company
+from career.users.models import User, BaseUser
 
 
 class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email', 'phone_number']
+
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'phone_number', 'address', 'description', 'image']
+
+class CompanyUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Company
+        fields = ['first_name', 'last_name', 'email', 'phone_number', 'name', 'description', 'industry', 'location', 'website', 'contact_email', 'logo', 'established_year']
