8a5c7bb783957e3e0129a7aecf499b0c3da5b54a
Dilyan Antonov <dilyan.y.antonov@gmail.com>
2024-07-15 23:29:03 +0300

    Update resume functionalities

--- career/career/resume/urls.py
+++ career/career/resume/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
-from .views import ResumeCreateView, ResumeDetailView, ProjectCreateView, ProjectListView, ProjectDeleteView
+from .views import (ResumeCreateView, ResumeDetailView, ProjectCreateView, ProjectListView, ProjectDeleteView,
+                    EducationCreateView, EducationListView, EducationDeleteView, ResumeUpdateView, ProjectUpdateView)
 
 app_name = 'resume'
 
 urlpatterns = [
     path('resume/', ResumeDetailView.as_view(), name='resume-detail'),
     path('create-resume/', ResumeCreateView.as_view(), name='create-resume'),
+    path('update-resume/', ResumeUpdateView.as_view(), name='update-resume'),
     path('projects/', ProjectListView.as_view(), name='projects-list'),
     path('create-project/', ProjectCreateView.as_view(), name='create-project'),
+    path('projects/<int:pk>/update/', ProjectUpdateView.as_view(), name='update-project'),
     path('delete/<int:pk>/', ProjectDeleteView.as_view(), name='delete-project'),
+    path('education/', EducationListView.as_view(), name='education-list'),
+    path('create-education/', EducationCreateView.as_view(), name='create-education'),
+    path('education/delete/<int:pk>/', EducationDeleteView.as_view(), name='delete-education'),
 ]
--- career/career/resume/views.py
+++ career/career/resume/views.py
@@ -1,7 +1,7 @@
-from django.views.generic import CreateView, ListView, DeleteView, DetailView
+from django.views.generic import CreateView, ListView, DeleteView, DetailView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from .models import Resume, Project
-from .forms import ResumeForm, ProjectForm
+from .models import Resume, Project, Education
+from .forms import ResumeForm, ProjectForm, EducationForm
 from django.urls import reverse_lazy
 
 
@@ -27,6 +27,23 @@ class ResumeDetailView(LoginRequiredMixin, DetailView):
             return None
 
 
+class ResumeUpdateView(LoginRequiredMixin, UpdateView):
+    model = Resume
+    form_class = ResumeForm
+    template_name = 'resume/resume_update.html'
+    success_url = reverse_lazy('resume:resume-detail')
+
+    def get_object(self, queryset=None):
+        try:
+            return self.request.user.resume
+        except Resume.DoesNotExist:
+            return None
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
 class ProjectCreateView(LoginRequiredMixin, CreateView):
     model = Project
     form_class = ProjectForm
@@ -38,6 +55,16 @@ class ProjectCreateView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
+class ProjectUpdateView(LoginRequiredMixin, UpdateView):
+    model = Project
+    form_class = ProjectForm
+    template_name = 'projects/project_update.html'
+    success_url = reverse_lazy('resume:projects-list')
+
+    def get_queryset(self):
+        return self.model.objects.filter(user=self.request.user)
+
+
 class ProjectListView(LoginRequiredMixin, ListView):
     model = Project
     context_object_name = 'projects'
@@ -47,6 +74,31 @@ class ProjectListView(LoginRequiredMixin, ListView):
 class ProjectDeleteView(LoginRequiredMixin, DeleteView):
     model = Project
     success_url = reverse_lazy('resume:projects-list')
+
+    def get_queryset(self):
+        return self.model.objects.filter(user=self.request.user)
+
+
+class EducationCreateView(LoginRequiredMixin, CreateView):
+    model = Education
+    form_class = EducationForm
+    template_name = 'resume/education_create_form.html'
+    success_url = reverse_lazy('resume:education-list')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class EducationListView(LoginRequiredMixin, ListView):
+    model = Education
+    context_object_name = 'educations'
+    template_name = 'resume/education_list.html'
+
+
+class EducationDeleteView(LoginRequiredMixin, DeleteView):
+    model = Education
+    success_url = reverse_lazy('resume:education-list')
 
     def get_queryset(self):
         return self.model.objects.filter(user=self.request.user)
