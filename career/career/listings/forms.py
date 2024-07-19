7a5cad0c3139e77df068ce4a506f2466d3bc5202
Dilyan Antonov <dilyan.y.antonov@gmail.com>
2024-07-20 00:35:44 +0300

    Update job listings

--- career/career/listings/forms.py
+++ career/career/listings/forms.py
@@ -6,11 +6,12 @@ class JobListingForm(forms.ModelForm):
     class Meta:
         model = JobListing
         fields = [
-            'company', 'title', 'description', 'requirements',
-            'location', 'employment_type', 'application_deadline', 'salary_range'
+            'title', 'description', 'requirements',
+            'location', 'employment_type', 'application_deadline', 'salary_min', 'salary_max'
         ]
         widgets = {
             'description': forms.Textarea(attrs={'rows': 4, 'cols': 15}),
             'requirements': forms.Textarea(attrs={'rows': 4, 'cols': 15}),
             'application_deadline': forms.DateInput(format='%Y-%m-%d', attrs={'type': 'date'}),
+            'employment_type': forms.Select(choices=JobListing.EMPLOYMENT_TYPE_CHOICES),
         }
--- career/career/listings/urls.py
+++ career/career/listings/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import JobListingListView, JobListingCreateView, JobListingDetailView
+from .views import JobListingListView, JobListingDetailView
 
 app_name = 'listings'
 
 urlpatterns = [
     path('', JobListingListView.as_view(), name='list'),
-    path('create/', JobListingCreateView.as_view(), name='create'),
     path('<int:pk>/', JobListingDetailView.as_view(), name='detail'),
 ]
